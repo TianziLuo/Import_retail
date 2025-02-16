9dc9e071e312cb7b93139a73c71abef9a558ed38
Sandy Luo <tluo023@ucr.edu>
2025-02-15 23:58:45 -0800

    Category data

--- Sheet_Item_Data.py
+++ Sheet_Item_Data.py
@@ -1,43 +1,53 @@
 import pandas as pd
-## Group_data
-def Group_data(df):
-    unique_department_Name_En = df['Department_En'].dropna().unique()
-    unique_department_Name_Cn = df['Department_Cn'].dropna().unique()
-    unique_department_id = df['Department_ID'].dropna().unique()
-    Group_data =  [list(unique_department_Name_En),
-                list(unique_department_id),
-                list(unique_department_Name_En),
-                list(unique_department_Name_Cn)]
+## Department_data
+def Department_data(df):
+    unique_Department_En = df['Department_En'].dropna().unique()
+    unique_Department_Cn = df['Department_Cn'].dropna().unique()
+    unique_Department_ID = df['Department_ID'].dropna().unique()
+    
+    ## name	sort status	en	cn
+    Department_data = [list(unique_Department_En),
+                  list(unique_Department_ID),          # Sort
+                  [1] * len(unique_Department_En),     # One row/ status
+                  list(unique_Department_En),
+                  list(unique_Department_Cn)]
+    
+    # Convert to DataFrame
+    Department_data = pd.DataFrame(list(zip(*Department_data)))
+    return Department_data
 
-    none_row = [None] * len(unique_department_Name_En)
-    one_row = [1] * len(unique_department_Name_En) 
+## Category_data
+def Category_data(df):
+    unique_Category_En = df['Cate_En'].dropna().unique()
+    unique_Category_Cn = df['Cate_Cn'].dropna().unique()
+    unique_Category_ID = df['Cate_ID'].dropna().unique()
+    unique_Category_is_tax_free = df['Cate_is_tax_free'].dropna()
 
-    for _ in range(2):
-        Group_data.insert(2, none_row.copy())
-    Group_data.insert(4, one_row.copy())
+    num_categories = len(unique_Category_En)
+    ## department_id	name	is_tax_free	font_color	bg_color	font_size	font_weight	sort	status	en	cn
+    Category_data = [list(unique_Category_ID),  ## ID
+                     list(unique_Category_En),  ## Name
+                     list(unique_Category_is_tax_free), ## is_tax_free
+                     ['#00000'] * len(unique_Category_En), ## font_color
+                     ['#FFFFFF'] * len(unique_Category_En), ## bg_color
+                     [None] * len(unique_Category_En), ## font_size
+                     [None] * len(unique_Category_En), ## font_weight sort
+                     [1] * len(unique_Category_En),     # One row/ status
+                    list(unique_Category_En),
+                    list(unique_Category_Cn)]
+    # Convert to DataFrame
+    Category_data = pd.DataFrame(list(zip(*Category_data)))
+    return Category_data
+   
+# Read the Excel file
+file_path = r"C:\Users\tianz\OneDrive\桌面\retail.xlsx"
+df = pd.read_excel(file_path, engine='openpyxl')
+# Call function
+result1 = Department_data(df)
+result2 = Category_data(df)
+print(result1,result2)
 
-    Group_data = pd.DataFrame(list(zip(*Group_data)))
-    return Group_data
-
-## Categories_data
-def Categories_data(df):
-    filter_condition = df['Cate_En'].notna()
-    filtered_data = df[filter_condition]
-
-    Categories_data = filtered_data.iloc[:, [0,4,3,4,5]]
-
-    columns_to_insert = {
-        'FontColor': pd.Series(['#FFFFFF'] * len(filtered_data), index=filtered_data.index),
-        'BackgroundColor': pd.Series(['#1FA3DC'] * len(filtered_data), index=filtered_data.index),
-        'OneCol': pd.Series([1] * len(filtered_data), index=filtered_data.index)
-        }
-
-    insert_index =3
-    for col_name, col_data in columns_to_insert.items():
-        Categories_data.insert(insert_index, col_name, col_data)
-        insert_index += 1
-    return Categories_data
-
+'''
 ## MenuItems_data
 def MenuItems_data(df):
     MenuItems_data = df.iloc[:, [7,9,10,6,6,7,8]]
@@ -59,3 +69,4 @@ def MenuItems_data(df):
 def CatalogyToItem_data(df):
     CatalogyToItem_data = df.iloc[:, [3,6,6]]
     return CatalogyToItem_data 
+'''
